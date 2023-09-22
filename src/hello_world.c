3211a79acf7789bb0bff4cb1d2bde8c5819c212e
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-09-22 17:39:47 +0200

    feat: use amboso 1.6.5, autotools template (#13)

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include "../static/amboso.h"
+#include "anvil__hello_world.h"
 
 int main(void) {
   printf("Hello, World!\n");
-  printf("\nAmboso template project, using lvl %s\n",getAmbosoVersion());
+  printf("\nAmboso template project, using lvl [%s]\n",get_ANVIL__API__LEVEL__());
+  printf("\nTemplate version: [%s]\n",get_ANVIL__VERSION__());
+  printf("\nTemplate version desc: [%s]\n",get_ANVIL__VERSION__DESC__());
+  printf("\nTemplate version date: [%s]\n",get_ANVIL__VERSION__DATE__());
+  printf("\nTemplate version author: [%s]\n",get_ANVIL__VERSION__AUTHOR__());
   return 0;
 }
